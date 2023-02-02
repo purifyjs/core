bb4de4b34b67891817dc2aba9fa2273130ffb0fa
Shiba <44804845+DeepDoge@users.noreply.github.com>
2023-02-02 19:36:08 +0000

    better each?

--- library/signal/each.ts
+++ library/signal/each.ts
@@ -2,13 +2,31 @@ import { SignalReadable } from "./readable"
 import { createDerive } from "./derivable"
 import { createWritable, SignalWritable } from "./writable"
 
-export function createEach<T extends unknown[], R>(each: SignalReadable<T> | T, as: (item: T[number], index: SignalReadable<number>) => R, key?: (item: T[number]) => string | number)
+type TypeOfEach<T> = T extends SignalReadable<infer U> ? U : T
+export function createEach<T extends unknown[] | SignalReadable<unknown[]>, R>(
+    each: T,
+    as: (item: TypeOfEach<T>[number], index: T extends SignalReadable ? SignalReadable<number> : number) => R,
+    ...key: T extends SignalReadable ? [(item: TypeOfEach<T>[number]) => string | number] : []
+): T extends SignalReadable ? SignalReadable<R[]> : R[]
+{
+    if (each instanceof SignalReadable) return (createEach_ArraySignal as any)(each, as, key) as any
+    return (createEach_Array as any)(each, as, key) as any
+}
+
+function createEach_Array<T extends unknown[], R>(each: T, as: (item: T[number], index: number) => R): R[]
+{
+    return each.map((item, index) => as(item, index))
+}
+
+function createEach_ArraySignal<T extends unknown[], R>(each: SignalReadable<T>, as: (item: T[number], index: SignalReadable<number>) => R, key?: (item: T[number]) => string | number): SignalReadable<R[]>
 {
     let caches: Record<string, { index: SignalWritable<number>, value: R }> = {}
-    const derived = createDerive(($) => {
+    const derived = createDerive(($) =>
+    {
         const newCaches: typeof caches = {}
         const collection = each instanceof SignalReadable ? $(each).value : each
-        const results = collection.map((item, index) => {
+        const results = collection.map((item, index) =>
+        {
             const k = key?.(item)
             if (k) 
             {
