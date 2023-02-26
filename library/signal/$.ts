35b5399a6ab227e395f91b2cae44ccbfb287bcb6
Shiba <44804845+DeepDoge@users.noreply.github.com>
2023-02-26 10:23:35 +0000

    ...

--- library/signal/$.ts
+++ library/signal/$.ts
@@ -1,9 +1,9 @@
-import { createWritable } from "./writable"
-import { createReadable } from "./readable"
-import { createDerive } from "./derive"
-import { createSwitch } from "./switch"
-import { createEach } from "./each"
 import { createAwait } from "./await"
+import { createDerive } from "./derive"
+import { createEach } from "./each"
+import { createReadable } from "./readable"
+import { createSwitch } from "./switch"
+import { createWritable } from "./writable"
 
 export const $ = {
 	writable: createWritable,
--- library/signal/switch.ts
+++ library/signal/switch.ts
@@ -1,34 +1,52 @@
 import { createDerive } from "./derive"
 import { SignalReadable } from "./readable"
 
-type ThenUnknown = () => unknown
-type CaseUnknown = unknown
-// type IfUnknown = (value: unknown) => unknown
+type IsLiteral<T, Then, Else> = T extends number
+	? number extends T
+		? Else
+		: Then
+	: T extends string
+	? string extends T
+		? Else
+		: Then
+	: T extends symbol
+	? symbol extends T
+		? Else
+		: Then
+	: T extends boolean
+	? boolean extends T
+		? Else
+		: Then
+	: Else
 
-interface Switch<T extends SignalReadable<unknown> | unknown, Returns> {
-	case<Case extends CaseUnknown, Then extends ThenUnknown>(value: Case, then: Then): Switch<T, Returns | ReturnType<Then>>
-	// if<If extends IfUnknown, Then extends ThenUnknown>(if_: If, then_: Then): Switch<Returns | ReturnType<Then>>
+type ThenUnknown = () => unknown
+
+type Switch<T, Returns, IsSignal extends boolean> = {
+	case<Case extends T, Then extends ThenUnknown>(
+		value: Case,
+		then: Then
+	): Switch<IsLiteral<Case, Exclude<T, Case>, T>, Returns | ReturnType<Then>, IsSignal>
 	$<Default extends ThenUnknown>(
-		fallback?: Default
+		...fallback: T extends never ? never : [Default]
 	): Default extends unknown
-		? T extends SignalReadable<any>
+		? IsSignal extends true
 			? SignalReadable<Returns>
 			: Returns
-		: T extends SignalReadable<any>
+		: IsSignal extends true
 		? SignalReadable<Returns | ReturnType<Default>>
 		: Returns | ReturnType<Default>
 }
 
-export function createSwitch<T extends SignalReadable<unknown> | unknown>(match: T): Switch<T, never> {
-	const cases = new Map<CaseUnknown, ThenUnknown>()
+export function createSwitch<T>(value: T): Switch<T extends SignalReadable<infer U> ? U : T, never, T extends SignalReadable<any> ? true : false> {
+	const cases = new Map<unknown, ThenUnknown>()
 	return {
 		case(value, then) {
 			cases.set(value, then)
 			return this as never
 		},
-		$(fallback) {
-			if (match instanceof SignalReadable) return createDerive((s) => cases.get(s(match).ref)?.() ?? fallback?.() ?? null) as never
-			return (cases.get(match)?.() ?? fallback?.() ?? null) as any
+		$(fallback?: ThenUnknown) {
+			if (value instanceof SignalReadable) return createDerive((s) => cases.get(s(value).ref)?.() ?? fallback?.() ?? null) as never
+			return (cases.get(value)?.() ?? fallback?.() ?? null) as never
 		},
 	}
 }
