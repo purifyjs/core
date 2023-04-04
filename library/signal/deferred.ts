89e0add31c4edd46581c595956f5d38a32cc76c7
Shiba <44804845+DeepDoge@users.noreply.github.com>
2023-04-04 23:14:25 +0000

    better? i think its suppose to be like this

--- library/signal/deferred.ts
+++ library/signal/deferred.ts
@@ -3,12 +3,9 @@ import { createReadable, SignalReadable } from "../signal/readable"
 export function createDeferred<T>(signal: SignalReadable<T>, timeout_ms: number = 1000): SignalReadable<T> {
 	return createReadable<T>(signal.ref, (set) => {
 		let timeout: number | null = null
-		return signal.subscribe(() => {
-			if (timeout) return
-			timeout = setTimeout(() => {
-				timeout = null
-				set(signal.ref)
-			}, timeout_ms)
+		return signal.subscribe((value) => {
+			if (timeout) clearTimeout(timeout)
+			timeout = setTimeout(() => set(value), timeout_ms)
 		}).unsubscribe
 	})
 }
