f53acdd31689e5f7b9cebfcc6480d4e4f75dfe0a
Shiba <44804845+DeepDoge@users.noreply.github.com>
2023-05-10 12:12:17 +0000

    oops

--- library/signal/deferred.ts
+++ library/signal/deferred.ts
@@ -2,10 +2,10 @@ import { createReadable, type SignalReadable } from "."
 
 export function createDeferred<T>(signal: SignalReadable<T>, timeout_ms: number = 500): SignalReadable<T> {
 	return createReadable<T>((set) => {
-		let timeout: number | null = null
+		let timeout: NodeJS.Timeout | null = null
+		set(signal.get(true))
 		return signal.subscribe(
 			(value) => {
-				set(value)
 				if (timeout) clearTimeout(timeout)
 				timeout = setTimeout(() => set(value), timeout_ms)
 			},
