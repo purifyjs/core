398ead8b83c8720cd0437b8757d8acc72a5fcdf4
Shiba <44804845+DeepDoge@users.noreply.github.com>
2023-05-17 10:15:11 +0000

    should fix it ?

--- library/utils/type.ts
+++ library/utils/type.ts
@@ -3,17 +3,25 @@ export type Obj = Record<PropertyKey, any>
 export type Excludable<T, Then, Else> = T extends number
 	? number extends T
 		? Else
+		: T extends object
+		? Else
 		: Then
 	: T extends string
 	? string extends T
+		? Else
+		: T extends object
 		? Else
 		: Then
 	: T extends symbol
 	? symbol extends T
 		? Else
+		: T extends object
+		? Else
 		: Then
 	: T extends boolean
 	? boolean extends T
+		? Else
+		: T extends object
 		? Else
 		: Then
 	: T extends Function
