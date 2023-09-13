20d2fa48cedeeaa5521f053702cdbaf31a26b56d
Shiba <44804845+DeepDoge@users.noreply.github.com>
2023-09-13 20:05:26 +0000

    btw this is not have to be a signal

--- app/libs/ipfs.ts
+++ app/libs/ipfs.ts
@@ -1,9 +1,7 @@
-import { derive } from "@/lib/core.ts"
-
 export namespace IPFS {
 	export const isIpfs = location.hostname.endsWith("ipfs.localhost") || location.hostname.endsWith("ipns.localhost")
 
 	export function resolve(hash: string) {
-		return derive(() => (isIpfs ? `ipfs://${hash}` : `https://ipfs.io/ipfs/${hash}`))
+		return isIpfs ? `ipfs://${hash}` : `https://ipfs.io/ipfs/${hash}`
 	}
 }
