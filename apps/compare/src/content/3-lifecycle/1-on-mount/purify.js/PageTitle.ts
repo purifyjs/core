16bf2d4674b74b13ad540e2501395f633e0ea999
Shiba <44804845+DeepDoge@users.noreply.github.com>
2024-10-16 11:32:58 +0000

    Updated PageTitle example

--- apps/compare/src/content/3-lifecycle/1-on-mount/purify.js/PageTitle.ts
+++ apps/compare/src/content/3-lifecycle/1-on-mount/purify.js/PageTitle.ts
@@ -3,12 +3,11 @@ import { ref, tags } from "@purifyjs/core";
 const { p } = tags;
 
 export function PageTitle() {
-	const host = p();
-
 	const pageTitle = ref("");
-	host.element.onConnect(() => {
-		pageTitle.val = document.title;
-	});
 
-	return host.children(pageTitle);
+	return p()
+		.use(() => {
+			pageTitle.val = document.title;
+		})
+		.children(pageTitle);
 }
